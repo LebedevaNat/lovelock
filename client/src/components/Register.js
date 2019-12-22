9befa5ba1ddfe494bb10fe34d3a9bd613070544a
atakhanova.an <atakhanova.it-11@mail.ru>
2019-12-23 01:16:04 +0300

    Поправила регистрацию

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -19,25 +19,25 @@ class Register extends Component {
                     <div class="row">                
                         <div class="card">
                             <div class="card-body">
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">Имя</label>
-                                    <input type="text" id="name" class="form-control"></input>
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">Фамилия</label>
-                                    <input type="text" id="surname" class="form-control"></input>
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">Email</label>
-                                    <input type="text" id="email" class="form-control"></input>
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleInputPassword1">Пароль</label>
-                                    <input type="text" id="password" class="form-control"></input>
-                                </div>
-                                <div class="form-group">
-                                    <button type="submit" class="btn btn-default" onClick={this.clickHandler}>Зарегистрироваться</button>
-                                </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Имя</label>
+                                        <input type="text" id="name" class="form-control"></input>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Фамилия</label>
+                                        <input type="text" id="surname" class="form-control"></input>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Логин</label>
+                                        <input type="text" id="email" class="form-control"></input>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">Пароль</label>
+                                        <input type="password" id="password" class="form-control"></input>
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-default" onClick={this.clickHandler}>Зарегистрироваться</button>
+                                    </div>
                             </div> 
                         </div>  
                     </div> 
