4bab68f1ee6b0ea89aed4a3885603c0d139350d8
atakhanova.an <atakhanova.it-11@mail.ru>
2019-12-23 01:44:21 +0300

    Изменен файл с авторизацией

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,29 +1,42 @@
 import React, {Component} from 'react';
 import {sendInputValue} from "../api/InputValues";
-import AuthForm from "./AuthForm";
 import '../style/auth.css';
 
 
 class Auth extends Component {
-    clickHandler() {
-        const name = document.getElementById("name").value;
-        const surname = document.getElementById("surname").value;
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
-        sendInputValue(name, surname, email, password);
-    }
+    // clickHandler() {
+    //     const email = document.getElementById("email").value;
+    //     const password = document.getElementById("password").value;
+    //     sendInputValue(email, password);
+    // }
 
     render() {
         return (
-        <div class="container">
-            <div class="row">
-                
-                    <div class="card">
-                        <div class="card-body">
-                            <AuthForm/>
-                        </div>                   
-                </div>
-            </div>
+            <div class="container">
+            <div class="row">                
+                <div class="card">
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Логин</label>
+                            <input type="text" class="form-control" id="email"></input>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Пароль</label>
+                            <input type="password" class="form-control" id="password"></input>
+                        </div>
+                        <div class="form-group">
+                            <div class="main-checkbox">
+                                <input value="None" id="checkbox1" name="check" type="checkbox"></input>
+                                <label for="checkbox1"></label>
+                            </div>
+                            <span class="text">Запомнить</span>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-default">Войти</button>
+                        </div>                            
+                    </div> 
+                </div>  
+            </div> 
         </div>
         );
     }   
